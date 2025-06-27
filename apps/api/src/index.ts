327ea767194a07791fee083ae6335e55c4206178
Aswin-04 <e.aswin004@gmail.com>
2025-06-27 21:54:54 +0530

    fix: change port number for api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config"
 import express from "express";
 import AuthRouter from './routes/auth'
 
-const PORT = process.env.PORT || 3000 
+const PORT = process.env.PORT || 3001
 
 const app = express()
 
