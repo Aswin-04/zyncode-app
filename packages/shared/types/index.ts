03216bfbb2c57c466a6c2472f7f04a30b5b66bea
Aswin-04 <e.aswin004@gmail.com>
2025-07-18 13:12:14 +0530

    create shared package

---
+++ packages/shared/types/index.ts
@@ -0,0 +1,47 @@
+interface CreateRoomPayload {
+  type: "create",
+}
+
+interface JoinRoomPayload {
+  type: "join",
+  payload: {
+    roomId: string
+  }
+}
+
+interface LeaveRoomPayload {
+  type: "leave",
+  payload: {
+    roomId: string
+  }
+}
+
+interface CodeChangePayload {
+  type: "codeChange",
+  payload: {
+    code: string
+  }
+}
+
+interface BaseResponse {
+  type: "response",
+  eventType: "room:create" | "room:join" | "room:leave" | "room:codeChange" | "room:submit" | "solo:init" | "solo:submit" 
+  success: boolean
+}
+
+interface SuccessResponse<T=any> extends BaseResponse {
+  success: true,
+  data: T
+}
+
+interface ErrorResponse extends BaseResponse {
+  success: false,
+  error: {
+    message: string
+  }
+}
+
+export type WSClientRequest = | CreateRoomPayload | JoinRoomPayload | LeaveRoomPayload | CodeChangePayload
+export type WSResponse<T=any> = SuccessResponse<T> | ErrorResponse
+
+
