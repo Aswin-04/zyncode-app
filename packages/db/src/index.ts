86cf127f644a1cef792a9bf6bdc3e6428a03c0d7
Aswin-04 <e.aswin004@gmail.com>
2025-07-18 18:53:13 +0530

    add build script

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -5,7 +5,7 @@ if(!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set in the environment");
 }
 
-const pool = new Pool({connectionString: process.env.DATABASE_URL})
+const pool = new Pool({connectionString: process.env.DATABASE_URL, ssl: {rejectUnauthorized: false}})
 
 export const query = (config: QueryConfig) => {
   return pool.query(config)
