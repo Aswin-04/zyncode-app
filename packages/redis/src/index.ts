27a8b3b3abc644213670e75e2a628c84ee3bcd30
Aswin-04 <e.aswin004@gmail.com>
2025-09-16 20:14:27 +0530

    refactor(redis): add dedicated Redis subscriber client

--- packages/redis/src/index.ts
+++ packages/redis/src/index.ts
@@ -2,22 +2,30 @@ import { Redis } from "ioredis";
 import "dotenv/config";
 
 let redisClient: Redis | null = null;
+let redisSubscriber: Redis | null = null;
 
-export async function getRedisClient(): Promise<Redis> {
+async function createRedisClient(): Promise<Redis> {
+
   const redisUrl = process.env.REDIS_URL;
 
   if (!redisUrl) {
     throw new Error("REDIS_URL is not defined in env");
   }
 
+  return new Redis(
+    redisUrl,
+    {tls: redisUrl.startsWith("rediss://") ? {} : undefined}
+  )
+}
+
+export async function getRedisClient(): Promise<Redis> {
+
+
   if (redisClient) {
     return redisClient;
   }
 
-  
-  redisClient = new Redis(redisUrl, {
-    tls: redisUrl.startsWith("rediss://") ? {} : undefined, 
-  });
+  redisClient = await createRedisClient()
 
   redisClient.on("connect", () => {
     console.log("redis connected");
@@ -28,4 +36,24 @@ export async function getRedisClient(): Promise<Redis> {
   });
 
   return redisClient;
+}
+
+export async function getRedisSubscriber(): Promise<Redis> {
+
+
+  if (redisSubscriber) {
+    return redisSubscriber;
+  }
+
+  redisSubscriber = await createRedisClient()
+
+  redisSubscriber.on("connect", () => {
+    console.log("redis subscriber connected");
+  });
+
+  redisSubscriber.on("error", (err) => {
+    console.error("redis subscriber error", err);
+  });
+
+  return redisSubscriber;
 }
