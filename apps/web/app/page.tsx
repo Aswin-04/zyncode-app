cd1dfefdc42417d670a1ac738ea984166f0566ab
Aswin-04 <e.aswin004@gmail.com>
2025-10-17 07:13:13 +0530

    add input and output textarea in ui

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,17 +1,43 @@
 'use client'
-  import CodeEditor from "@/components/editor";
-  import Header from "@/components/header";
-  import { useCurrentUser } from "@/lib/providers/current-user";
-  import { useEffect, useRef } from "react";
+import CodeEditor from "@/components/editor";
+import Header from "@/components/header";
+import { useCurrentUser } from "@/lib/providers/current-user";
+import { useEffect, useRef, useState, memo } from "react";
+import { Textarea } from "@/components/ui/textarea";
+import IOPanel from "@/components/io-panel";
+
+import { useWebSocket } from '@/lib/providers/web-socket'
+import Editor, { OnChange } from '@monaco-editor/react'
+import { WSClientRequest, WSResponse} from "@repo/shared/types"
+import { Button } from "@/components/ui/button";
 
 
-  export default function Home() {
+export default function Home() {
+
+  const [code, setCode] = useState<string>('')
+  const [input, setInput] = useState<string>('')
+  const [executionResult, setExecutionResult] = useState<{username: string, stdin: string, stdout: string} | null>(null)
+  const [roomId, setRoomId] = useState<string | null>(null)
 
 
-    return (
-      <div>
-        <Header></Header>
-        <CodeEditor></CodeEditor>
+  return (
+    <div>
+      <Header roomId={roomId} setRoomId={setRoomId} ></Header>
+      <div className="flex">
+        <div className=" flex-1 border-rose-500">
+          <CodeEditor code={code} setCode={setCode}></CodeEditor>
+        </div>
+        <div className="flex-1 h-[90vh]">
+          <IOPanel 
+            code={code}
+            input={input}
+            executionResult={executionResult}
+            roomId={roomId}
+            setInput={setInput}
+            setExecutionResult={setExecutionResult}
+          ></IOPanel>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
---
+++ apps/web/components/io-panel.tsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useRef } from 'react'
+import { Textarea } from './ui/textarea'
+import { Button } from './ui/button'
+import { useState } from 'react'
+import { useWebSocket } from '@/lib/providers/web-socket'
+import { WSResponse } from '@repo/shared/types'
+import { useCurrentUser } from '@/lib/providers/current-user'
+
+interface ExecutionResult {
+  username: string, 
+  stdin: string,
+  stdout: string
+} 
+  
+interface IOPanelProps {
+  code: string
+  input: string;
+  roomId: string | null, 
+  executionResult: ExecutionResult | null 
+  setInput: (input: string) => void;
+  setExecutionResult: (result: ExecutionResult | null) => void
+}
+
+const isBinary = (message: unknown): message is Blob => {
+  return typeof message === 'object' && Object.prototype.toString.call(message) === '[object Blob]' && message instanceof Blob
+}
+
+const IOPanel = ({code, input, executionResult, roomId, setInput, setExecutionResult}: IOPanelProps) => {
+
+  const user = useCurrentUser()
+
+  const { ws } =  useWebSocket()
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null)
+
+  const executeCode = async () => {
+    if(!user) {
+      console.error("User should login/signup to execute the code.")
+      return
+    }
+    try {
+      console.log(code)
+      const response = await fetch("/api/submit", {
+        method: 'POST',
+        body: JSON.stringify({
+          userId: user.userId, 
+          username: user.name,
+          roomId: roomId,
+          language: "cpp",
+          code,
+          stdin: input
+        })
+      })
+    }
+
+    catch(err) {
+      console.error("Failed to execute code")
+    }
+  }
+
+  useEffect(() => {
+    const handleMessage = (event: MessageEvent<unknown>) => {
+      if(isBinary(event.data)) return
+      const message: WSResponse  = JSON.parse(event.data as string)
+      if(!message.success) {
+        console.log(message.error.message)
+        return
+      } 
+      
+      if(message.eventType === 'execution:result') {
+        console.log(message.data)
+        const {username, stdin, stdout}: ExecutionResult = message.data
+        setExecutionResult({username, stdin, stdout})
+      }
+    }
+
+    ws?.addEventListener('message', handleMessage)
+
+    return () => {
+      if(ws) ws.removeEventListener('message', handleMessage)
+      if(timeoutRef.current) clearTimeout(timeoutRef.current)
+    }
+
+  }, [ws])
+
+  return (
+    <div className="h-full flex flex-col">
+      <div className="p-2">
+        <Button onClick={executeCode}>Run</Button>
+      </div>
+      {/* Input + Output split equally */}
+      <div className='flex-1 flex flex-col'>
+        <Textarea className="flex-1 resize-none" placeholder="Program input..." onChange={(e) => setInput(e.target.value)} />
+        <div className="p-10 bg-input/30 text-gray-50 flex-1 border">
+        {executionResult 
+          ? <OutputDisplay {...executionResult}></OutputDisplay>
+          : ""
+        }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const OutputDisplay = ({ username, stdin, stdout }: {username: string, stdin: string, stdout: string}) => {
+  return (
+    <div className="space-y-4">
+      <div className="text-sm text-muted-foreground">
+        Executed by: <span className="font-medium text-white">{username}</span>
+      </div>
+      
+      <div>
+        <div className="text-sm font-medium mb-2">Input:</div>
+        <pre className="bg-muted p-3 rounded text-sm whitespace-pre-wrap">{stdin}</pre>
+      </div>
+      
+      <div>
+        <div className="text-sm font-medium mb-2">Output:</div>
+        <pre className="bg-muted p-3 rounded text-sm whitespace-pre-wrap">{stdout}</pre>
+      </div>
+    </div>
+  );
+};
+
+export default IOPanel
